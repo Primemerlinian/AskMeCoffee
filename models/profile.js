5a04fc2109320f63aa8cb6366efa45508e36f63c
Primemerlinian <mwatson0518@gmail.com>
2023-01-18 20:11:51 -0500

    add drinkbase field in drink info

--- models/profile.js
+++ models/profile.js
@@ -5,6 +5,10 @@ const Schema = mongoose.Schema
 const profileSchema = new Schema({
   name: String,
   avatar: String,
+  drinks: [{
+    type: Schema.Types.ObjectId,
+    ref: 'Drink'
+  }]
 }, {
   timestamps: true
 })
